2f478e5a2b6fc9d3e0bf176e795093d3e482ffa8
Syed Amber Iqbal <syiqbal@deloitte.com>
2016-10-04 10:15:08 +0530

    Commit a new function in index.js which overwrites PythonShell page

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var username = 'mmunavarali@gso3.lly.deldev3';
 var password = 'Ammijaan_5254';
 var express = require('express');
 var fs = require('fs');
-
+var fileType = 'application/pdf';
 var conn = new jsforce.Connection({
   oauth2 : {
     loginUrl : 'https://test.salesforce.com',
@@ -16,17 +16,10 @@ var conn = new jsforce.Connection({
   }
 });
 
-router.post('/node',function(req,res,next){
-   //router.set('json_input',json_input);
-   console.log('Req body--->'+req.body);
-   console.log('Req body structure--->'+req.body.requestparam);
-   res.redirect(200,'/node/'+req.body.requestparam);//Redirect the request ... requestparam is the json request which will be sent from salesforce
-});
-
 /* GET home page. */
-router.get('/node/:jsonAttachment', function(req, res, next) {
-  console.log('....'+req.params.jsonAttachment);
-  json_input = JSON.parse(req.params.jsonAttachment); //parse json request coming from salesforce
+router.post('/node', function(req, res, next) {
+  console.log('....'+req.body.requestparam);
+  var json_input = JSON.parse(req.body.requestparam); //parse json request coming from salesforce
   console.log('...123...'+json_input.attachmentId)
   conn.login(username, password, function(err, userInfo) {//connect with salesforce
   	  if (err) { return console.error(err); }
@@ -35,12 +28,91 @@ router.get('/node/:jsonAttachment', function(req, res, next) {
 		  conn.sobject('Attachment').record(json_input.attachmentId).blob('Body').pipe(fileOut)
 		  .on('finish',function(){//upon write finish
           console.log('Done downloading the file.');
-          res.redirect('/PythonShell/'+req.params.jsonAttachment);
+          callPythonShell(req.body.requestparam);
+          res.send('Process Complete');
       })
       .on('error', function(err){//upon write error
 			    console.log('ERROR!!!');
+         
       });
   });//end of connection	
 });
 
+function callPythonShell(jsonbody){
+  var options = {mode: 'text',pythonOptions: ['-u'],args: []};//set options argument for Python code
+	options.args.push(jsonbody);//Push jsonrequest to the python code
+	var json_input = JSON.parse(jsonbody); //parse json request coming from salesforce
+  var requiredsplits = json_input.pdftocreate;
+	var parentdoc = json_input.parentdocid;
+	console.log('Inside Code'+options);
+  //Make a call to python code
+	PythonShell.run('splitpython.py', options, function (err, results) {
+    if (err) throw err;
+   // results is an array consisting of messages collected during execution
+     console.log('results: %j', results);
+  });//End of python code 
+
+  conn.login(username, password, function(err, userInfo) {//Connect with salesforce to upload attachment
+		if (err) { return console.error(err); }//throw error if login failed
+		console.log(userInfo);//log userInfo from salesfore
+		
+		//start reading root directory to check the file extensions
+		fs.readdir('./', function(err, files) {
+			if (err) return;//throw upon exception
+			files.forEach(function(f) {
+			//Start iteration over files in the root to check if the file is a split
+				console.log(f);
+				if(f.indexOf('split.pdf')>=0)
+				{
+					//conn.sobject('DTPC_Document__c').create({ RecordTypeId: '012600000001FNTAA2' }, function(err, ret) {
+						//Callback to create document record in salesforce org
+						//if (err || !ret.success) { return console.error(err, ret); }
+						//console log document id upon creation
+						//console.log("Created record id : " + ret.id);
+						var filename = f;
+						fs.readFile(filename, function (err, filedata) {
+						//Start of split file read and attachment upload
+									if (err){
+												console.error(err);
+									}
+									else{
+												var title = filename.substring(0,filename.indexOf('_'));
+												var targetId = filename.substring(filename.indexOf('_')+1,filename.indexOf('-'));
+												console.log(filedata);//Upload attachment code
+												var base64data = new Buffer(filedata).toString('base64');
+												conn.sobject('Attachment').create({
+													ParentId: targetId,
+													Name : title,
+													Body: base64data,
+													ContentType : fileType,
+												},
+												//Throw an exception in case exception fails
+												function(err, uploadedAttachment) {
+														console.log(err,uploadedAttachment);
+														if(uploadedAttachment!=null)
+															requiredsplits--;
+														if(requiredsplits == 0)
+															conn.sobject("DTPC_Document__c").update({ //Update parent document
+																Id : parentdoc,
+																Fax_Status__c : 'Fax Split Complete'
+															},function(err, ret) {
+  															  	if (err || !ret.success) { return console.error(err, ret); }
+																	console.log('Opertion is complete with update to parent doc- '+ ret.id);
+ 																   	// ...
+															});
+												});//End of creation of attachments in Salesforce.
+									}
+							//End of split file read and attachment upload
+						});
+						//Delete the split file from the root directory
+						fs.unlink(filename, function(err){
+							if (err) throw err;
+							console.log(filename + " deleted.");
+						});//End of Delete of splits
+					//});//End of Document creation in salesforce
+				};//End of condition on 'split.pdf'
+			});//End of file forEach iteration
+		});//End of readdir
+	});//end of connection with salesforce
+}
 module.exports = router;
