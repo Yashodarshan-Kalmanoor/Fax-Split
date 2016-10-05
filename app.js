90f02774fda9200a438134e015c64fa0aeac6791
Syed Amber Iqbal <syiqbal@deloitte.com>
2016-10-05 13:36:36 +0530

    Commit event listeners to be set to value 0

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var bodyParser = require('body-parser');
 var jsforce = require('jsforce');
 var port = process.env.PORT || 3008;
 var app = express();
-
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+eventEmitter.setMaxListeners(0);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
