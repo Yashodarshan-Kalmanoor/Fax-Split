07a158f6cdb0d84d741a8c95b5ad81d296e9e7b4
Syed Amber Iqbal <syiqbal@deloitte.com>
2016-10-06 00:15:54 +0530

    Commit Event Emitters in devR1 branch

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var jsforce = require('jsforce');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 var port = process.env.PORT || 3008;
 var app = express();
 
+eventEmitter.setMaxListeners(0);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
